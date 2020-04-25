ab8fbbaff421ace74836ffd494f6619d88713f78
amanjadon54 <amanjadon54@gmail.com>
2020-04-25 22:01:06 +0530

    Core: added script with target

--- script/scriptCentime.sh
+++ script/scriptCentime.sh
@@ -4,7 +4,7 @@ mvn clean install
 
 cd /home/ec2-user/centimeGit/centimeGreetingService
 mvn clean install
-java -jar target/concatenation-application-1.0-SNAPSHOT.jar
+java -jar target/GreetingService-1.0-SNAPSHOT.jar
 
 cd /home/ec2-user/centimeGit/centimeConcatenationService
 mvn clean install
